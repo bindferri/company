bf9c6156fa09e8c9d043e1818d3399157a6cc4e0
bindferri <bindferri@gmail.com>
2023-11-14 00:08:36 +0100

    Changed route from get to post

--- routes/api.php
+++ routes/api.php
@@ -26,5 +26,5 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::prefix('/packages')->middleware('auth:sanctum')->group(function (){
     Route::get('/', [PackageController::class, 'index']);
     Route::post('/', [PackageController::class, 'store']);
-    Route::get('/register/{package}', [PackageController::class, 'register']);
+    Route::post('/register/{package}', [PackageController::class, 'register']);
 });
